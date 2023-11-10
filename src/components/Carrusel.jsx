a6d9f2dfad06b40dae3edc84d012954ee75e1174
Ydelator <ygamerzxy24@gmail.com>
2023-11-09 23:09:28 -0500

    Expositores agregados

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import {Swiper, SwiperSlide} from 'swiper/react'  
+import Andres from '../assets/Andres_Serje.jpeg'
+import Bisila from '../assets/Bisila_Bokoko.jpeg'
+import Carlos from '../assets/Carlos_De_La_Hoz.jpg'
+import Fausto from '../assets/Fausto_Colanna.jpeg'
+import Juan from '../assets/Juan_Jaramillo.jpg'
+import Kathy from '../assets/Kathy_Lopez.jpeg'
 
 import 'swiper/css'
 import 'swiper/css/effect-cube'
@@ -29,37 +35,50 @@ const Carrusel = () => {
         >
             <SwiperSlide>
               <div className="tarjeta">
-                <h2>Nombre de Expositor 1</h2>
-                <div className='icono'><ion-icon name="person-circle-outline"></ion-icon></div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reprehenderit facilis quasi odit quos nesciunt, vitae ullam officia esse ab at eius itaque amet maxime exercitationem. Repellat nulla numquam at?</p>
+                <h2>Andres Serje</h2>
+                <img className='expositor' src={Andres} alt="andres" />
+                <h4>CEO Sin Filtro Agencia & Dinklab</h4>
+                <p>"Hablemos sobre la industria de los eventos y como lograr dejar una huella en cada asistente".</p>
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="tarjeta">
-                <h2>Nombre de Expositor 2</h2>
-                <div className='icono'><ion-icon name="person-circle-outline"></ion-icon></div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reprehenderit facilis quasi odit quos nesciunt, vitae ullam officia esse ab at eius itaque amet maxime exercitationem. Repellat nulla numquam at?</p>
+                <h2>Bisila Bokoko</h2>
+                <img className='expositor' src={Bisila} alt="bisila" />
+                <h4>Invitada Internacional</h4>
+                <p>Mujer de negocios, emprendedora y filántropa y CEO de BBES, una agencia de desarrollo empresarial de Nueva York que representa y promueve marcas de mercados internacionales.</p>
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="tarjeta">
-                <h2>Nombre de Expositor 3</h2>
-                <div className='icono'><ion-icon name="person-circle-outline"></ion-icon></div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reprehenderit facilis quasi odit quos nesciunt, vitae ullam officia esse ab at eius itaque amet maxime exercitationem. Repellat nulla numquam at?</p>
+                <h2>Carlos De La Hoz</h2>
+                <img className='expositor' src={Carlos} alt="carlos" />
+                <h4>Egresado SENA</h4>
+                <p>"Quiero contarte sobre mi experiencia profesional como egresado SENA, en el mercado laboral de los eventos".</p>
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="tarjeta">
-                <h2>Nombre de Expositor 4</h2>
-                <div className='icono'><ion-icon name="person-circle-outline"></ion-icon></div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reprehenderit facilis quasi odit quos nesciunt, vitae ullam officia esse ab at eius itaque amet maxime exercitationem. Repellat nulla numquam at?</p>
+                <h2>Fausto Colonna</h2>
+                <img className='expositor' src={Fausto} alt="fausto" />
+                <h4>Event Decorator</h4>
+                <p>"Te espero para contarte sobre la importancia de la Decoración en los eventos y su constante evolución".</p>
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className="tarjeta">
-                <h2>Nombre de Expositor 5</h2>
-                <div className='icono'><ion-icon name="person-circle-outline"></ion-icon></div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reprehenderit facilis quasi odit quos nesciunt, vitae ullam officia esse ab at eius itaque amet maxime exercitationem. Repellat nulla numquam at?</p>
+                <h2>Juan Jaramillo</h2>
+                <img className='expositor' src={Juan} alt="juan" />
+                <h4>Director de la CLENA</h4>
+                <p>"Hablemos sobre Barranquilla y la búsqueda de su vocación como clúster de eventos y negocios".</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="tarjeta">
+                <h2>Kathy López</h2>
+                <img className='expositor' src={Kathy} alt="kathy" />
+                <h4>Jefa de la Oficina de Turismo Alcaldía de Barranquilla</h4>
+                <p>"Barranquilla, destino lider en turismo de eventos y negocios en Suramérica".</p>
               </div>
             </SwiperSlide>
         </Swiper>
