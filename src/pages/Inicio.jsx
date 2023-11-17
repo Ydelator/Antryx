2d4da7f93dfd5de829ff54f801bac1914c6ed21d
Ydelator <ygamerzxy24@gmail.com>
2023-11-16 19:52:37 -0500

    cambios

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Contador from '../components/Contador.jsx'
-import banner from '../assets/FLYER OFICIAL.png'
+import banner from '../assets/Flyer_oficial.png'
 import Carrusel from '../components/Carrusel.jsx'
 import Formulario from '../components/Formulario.jsx'
-import agenda from '../assets/AGENDA ACADEMICA.JPG'
+import agenda from '../assets/Agenda_academica.JPG'
 import {RViewer, RViewerTrigger} from 'react-viewerjs'
 
 const Inicio = () => { 
